2d0363d564f21243c802df9358f3a0ee91412930
genildocs <genildocs@gmail.com>
2023-04-20 04:21:55 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 const menuToggle = document.querySelector(".menu-toggle");
+const hidden = document.querySelector(".hidden");
+const navMenu = document.querySelector(".nav__menu");
 
 
 const menuOn = function(){
     menuToggle.classList.toggle("on");
+    hidden.classList.toggle("hidden");
+    navMenu.classList.toggle("display-on");
+   
 }
 
 menuToggle.addEventListener("click", menuOn);
